f46cab7d389004026e3953ac84317755746750d1
heiym <9540040+heiym@users.noreply.github.com>
2020-02-27 20:48:14 +0100

    webpack stuff and yarn2

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,60 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-let mode = process.env.NODE_ENV || "production";
-let prod = mode === "production";
+module.exports = env => {
+  let mode = env.NODE_ENV|| "production";
+  let prod = mode === "production";
 
-module.exports = {
-  entry: {
-    frontend: "./app/src/renderer/index.ts"
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist/renderer")
-  },
-  resolve: {
-    alias: {
-      svelte: path.resolve("node_modules", "svelte")
+  return {
+    entry: {
+      frontend: "./app/src/renderer/index.ts"
     },
-    extensions: [".ts", ".mjs", ".js", ".svelte"],
-    mainFields: ["svelte", "browser", "module", "main"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        loader: "ts-loader"
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist/renderer")
+    },
+    resolve: {
+      alias: {
+        svelte: path.resolve("node_modules", "svelte")
       },
-      {
-        test: /\.(html|svelte)$/,
-        exclude: "/node_modules/",
-        use: {
-          loader: "svelte-loader",
-          options: {
-            emitCss: true,
-            hotReload: true
+      extensions: [".ts", ".mjs", ".js", ".svelte"],
+      mainFields: ["svelte", "browser", "module", "main"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          include: path.resolve(__dirname, "app/src"),
+          loader: "ts-loader"
+        },
+        {
+          test: /\.(html|svelte)$/,
+          include: path.resolve(__dirname, "app/src/renderer"),
+          exclude: "/node_modules/",
+          use: {
+            loader: "svelte-loader",
+            options: {
+              emitCss: true,
+              hotReload: true
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            prod ? MiniCssExtractPlugin.loader: 'style-loader',
+            "css-loader"
+          ]
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          prod ? MiniCssExtractPlugin.loader: 'style-loader',
-          "css-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    })
-  ],
-  mode,
-  devtool: prod ? false : "source-map"
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
+      })
+    ],
+    mode,
+    devtool: prod ? false : "source-map"
+
+  }
 }
